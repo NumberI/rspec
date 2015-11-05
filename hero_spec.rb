874dff6b59714c9fbd24b0b4fb8b21e0bccdd94f
NumberI <x0987654321@ya.ru>
2015-11-05 18:34:26 +0300

    all tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,12 +1,27 @@
 require "./hero"
 describe Hero do
+
+  before do
+    @hero = Hero.new "mike"
+  end
+  
   it "has a capitalized name" do
-    hero = Hero.new "mike"
-    expect(hero.name).to eq "Mike"
+    
+    expect(@hero.name).to eq "Mike"
   end  
   
   it "can power up" do
-    hero = Hero.new "mike"
-    expect(hero.power_up).to eq 110
+    
+    expect(@hero.power_up).to eq 110
+  end  
+  
+  it "can power down" do
+    
+    expect(@hero.power_down).to eq 90
+  end
+  
+  it "displays info" do
+    
+    expect(@hero.hero_info).to eq "Mike has a health of 100"
   end
 end
